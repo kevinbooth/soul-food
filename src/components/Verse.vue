35869e1d582a6f9dd8424e0a6c81ef6258c68d48
Kevin Booth <kdbooth97@gmail.com>
2019-03-13 12:39:08 -0400

    Update verse component and stub out API calls

--- src/components/Verse.vue
+++ src/components/Verse.vue
@@ -1,32 +1,67 @@
 <template>
   <div class="verse">
-    <h1>{{ msg }}</h1>
+    <div class="verse__hero">
+      <div class="container">
+        <h1>verse of the day</h1>
+        <h2>john 3:16</h2>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { https } from "../axios.js";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      verse: "",
+      bibleVersion: "de4e12af7f28f599-01",
+      error: []
+    };
+  },
+  created() {
+    https
+      .get(
+        `bibles/${this.bibleVersion}/search?query=?${
+          this.randomVowel
+        }?&limit=10000&sort=${this.randomSort}`
+      )
+      .then(response => {
+        // console.log(response);
+        // console.log(response.data.data.verses[566])
+        this.verse =
+          response.data.data.verses[
+            Math.floor(Math.random() * response.data.verses.length)
+          ];
+        // console.log(this.verse)
+      })
+      .catch(e => {
+        this.error = e;
+      });
+  },
+  computed: {
+    randomVowel: () => {
+      return "aeiouy"[Math.floor(Math.random() * "aeiouy".length)];
+    },
+    randomSort: () => {
+      let sortOptions = ["reverse-canonical", "canonical", "relevance"];
+      return sortOptions[Math.floor(Math.random() * sortOptions.length)];
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.verse__hero {
+  background-color: #a0ddff;
+  font-family: monospace, sans-serif;
+  padding: 20px 0;
+  text-align: left;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Verse msg="Welcome to Your Vue.js App" />
   </div>
 </template>
