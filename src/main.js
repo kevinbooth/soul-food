5ec39e70317e6a82f3e4524bcae96446b63848d1
Kevin Booth <kdbooth97@gmail.com>
2019-03-13 11:17:46 -0400

    Add Verse component and delete HelloWorld component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+//Assets
+import "./assets/styles/skeleton.css";
+import "./assets/styles/normalize.css";
+
 new Vue({
   router,
   render: h => h(App)
